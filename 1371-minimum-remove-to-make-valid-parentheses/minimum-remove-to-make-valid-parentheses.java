e1b2c1ca5bef538846ce30d1237c68a849bb6abc
Phani <annadevaraphaneendra@gmail.com>
2026-07-27 17:07:03 +0530

    Time: 20 ms (53.38%) | Memory: 47.2 MB (69.46%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Stack<Integer> stack = new Stack<>();
+        boolean remove[] = new boolean[s.length()];
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='('){
+                stack.push(i);
+            }else if(ch==')'){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }else{
+                    remove[i] = true;
+                }
+            }
+        }
+        while(!stack.isEmpty()){
+            remove[stack.pop()] = true;
+        }
+        StringBuilder res = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(!remove[i]){
+                res.append(s.charAt(i));
+            }
+        }return res.toString();
+    }
+}
