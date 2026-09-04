f3afb687632fd4e1cdd66e83dda372db29b2ef4b
Phani <annadevaraphaneendra@gmail.com>
2026-09-05 00:22:58 +0530

    Time: 1 ms (87.88%) | Memory: 45.4 MB (62.58%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,27 +1,24 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
-    boolean visit[];
-    public List<List<Integer>> permute(int[] nums) {
-        visit = new boolean[nums.length];
-        solve(new ArrayList<>(),0,nums);
-        return ans;
-    }
-    
-    void solve(List<Integer>arr, int index,int[] nums){
-       
-        if(index==nums.length){
-            ans.add(new ArrayList<>(arr));
+    List<List<Integer>> res = new ArrayList<>();
+    // boolean visit[] = new boolean[nums.length];
+    public void recurse(int[] nums,List<Integer>arr, boolean[] visit){
+        if(arr.size()==nums.length){
+            res.add(new ArrayList<>(arr));
             return;
         }
-        
         for(int i=0;i<nums.length;i++){
-            if(visit[i]==false){
-                arr.add(nums[i]);
-                visit[i] = true;
-                solve(arr,index+1,nums);
-                arr.remove(arr.size()-1);
-                visit[i] = false;
-            }
+            if(visit[i]) continue;
+            arr.add(nums[i]);
+            visit[i] = true;
+            recurse(nums,arr,visit);
+            arr.remove(arr.size()-1);
+            visit[i] = false;
         }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        boolean visit[] = new boolean[nums.length];
+        recurse(nums,new ArrayList<>(),visit);
+        return res;
     }
 }
